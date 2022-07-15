6714e9f6c053a2ad3de89d26495a52ff12c9ffa2
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-15 12:11:09 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,39 @@
+# a=5
+# for i in range(a):
+#     print(i) # (0 to 4 in vertical)
+
+# a=5
+# for i in range(a):
+#     print(i,end='') # 01234 (in horizontal)
+
+# for i in range(1,8):
+#      print(i)  # (1 to 7 in vertical)
+
+# for i in range(5,6):
+#     print(i) # 5
+
+# for i in range(8,8):
+#       print(i) # it will not showing nothing
+
+# for i in range(0,8,2):
+#     print(i) # 0,2,4,6 in vertical
+
+# for i in range(1,8,2):
+#    print(i) # 1,3,5,7 in vertical
+
+# for i in range(6,0,-1):
+#     print(i) # 6 to 1 in vertical
+
+# for i in range(6,0,-2):
+#     print(i) # 6,4,2 in vertical
+
+
+# a=1
+# b=2
+# c=3
+# print(a,b,c,sep='*') # 1*2*3
+
+
+# a=(1,2,3,4,5)
+# print(*a) # 1 2 3 4 5
+
